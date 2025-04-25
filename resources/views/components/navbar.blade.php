47b549c9ebbdf51b6640254de57e82f8d6963a76
KarmaWT <mateus4pantoja@gmail.com>
2025-04-25 17:17:53 -0400

    Criado o componente da navbar, porem não foi testado ainda

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,15 @@
+<!-- NAVBAR -->
+<nav class="navbar">
+    <div class="navbar-left">
+        <img class="navbar-logo" src="ServiNow-Logo.png" alt="Logo" />
+        <ul>
+            <li><a href="#">Início</a></li>
+            <li><a href="#">Sobre nós</a></li>
+        </ul>
+    </div>
+
+    <div class="navbar-icons" style="padding-right: 1rem;">
+        <i class="fa-solid fa-bell"></i>
+        <i class="fa-solid fa-user"></i>
+    </div>
+</nav>
