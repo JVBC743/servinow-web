d2a511c2b4cad9cea5a592b55e1e118c1a45853a
jb <jb@jb-desktop-debian.jb-desktop-debian.com.br>
2025-05-12 17:03:56 -0400

    Usado os Media Queries para aplicar uma responsividade minima para a página 'Sobre Nós'

--- resources/views/pages/sobre-nos.blade.php
+++ resources/views/pages/sobre-nos.blade.php
@@ -4,25 +4,41 @@
     <title>Sobre Nós</title>
     <link rel="stylesheet" href="{{ asset('css/photos.css') }}"> <!-- Verificar se precisa usar o vite ou não -->
     <link rel="stylesheet" href="{{  asset('css/fonts-and-texts.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/default_page_style.css') }}">
 </head>
 <body>
     <p class = "big_text">Sobre Nós</p>
+    
     <p id = "text_general">Olá! Somos um grupo sem fins lucrativos, afinal, o que importa para nós é aquela nota boa que devemos ter no final do semestre! Brincadeiras a parte, buscamos oferecer os melhores sistemas para cada contexto que os nossos clientes (no caso, o professor) solicitarem. Além disso, se quiser, revise os 
-        <a href="{{ route('termos') }}">Termos de Compromisso e de Privacidade</a> que o site oferece aos nossos clientes.</p>
+        <a href="{{ route('termos') }}" class = "links">Termos de Compromisso e de Privacidade</a> que o site oferece aos nossos clientes.</p>
     <p class = "big_text">Integrantes</p>
     <div id = "photos">
-        <a href="https://github.com/KarmaWT">
-            <img class = "photo-1" src="{{ asset('images/pantoja.png') }}" alt="Foto do Matheus Pantoja">
-        </a>
-        <a href="https://github.com/JClaion">
-            <img class = "photo-1" src="{{ asset('images/claion.png') }}" alt="Foto do José Claion">    
-        </a>
-        <a href="">
-            <img class = "photo-2" src="{{ asset('images/manoel.png') }}" alt="Foto do Manoel Aguiar">
-        </a>
-        <a href="https://www.canva.com/design/DAF-2lbgVCA/ImVa15Jxaakq0Ii2Zk2tag/edit?utm_content=DAF-2lbgVCA&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton">
-            <img class = "photo-2" src="{{ asset('images/joao.png') }}" alt="Foto do João Victor">
-        </a>
+        <div class = "div-name-photo">
+            <a href="https://github.com/KarmaWT" target="_blank">
+                <img class = "photo-1" src="{{ asset('images/pantoja.png') }}" alt="Foto do Matheus Pantoja">
+            </a><br>
+            <p>Matheus Pantoja de Morais</p>
+        </div>
+        <div class = "div-name-photo">        
+            <a href="https://github.com/JClaion" target="_blank">
+                <img class = "photo-1" src="{{ asset('images/claion.png') }}" alt="Foto do José Claion">    
+            </a><br>
+            <p>José Claion Martins de Sousa</p>
+        </div>
+        <div class = "div-name-photo">
+            <a href="" target="_blank">
+                <img class = "photo-2" src="{{ asset('images/manoel.png') }}" alt="Foto do Manoel Aguiar">
+            </a><br>
+            <p>Manoel de Jesus Moreira de Aguiar</p>
+        </div>
+        <div class = "div-name-photo">
+            <a href="https://www.canva.com/design/DAF-2lbgVCA/ImVa15Jxaakq0Ii2Zk2tag/edit?utm_content=DAF-2lbgVCA&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton" target="_blank">
+                <img class = "photo-2" src="{{ asset('images/joao.png') }}" alt="Foto do João Victor">
+            </a><br>
+            <p>João Victor Brum de Castro</p>
+        </div>
     </div>
+
+    <h1>Mudar a altura dos nomese do photo-2</h1>
 </body>
 </html>
