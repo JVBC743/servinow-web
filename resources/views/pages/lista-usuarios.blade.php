205d69f3b2bb573bb5a59b2279d18ff00c098642
jb <jb@jb-desktop-debian.jb-desktop-debian.com.br>
2025-05-23 16:51:51 -0400

    Aplicando a base do Bootstrap para a lista de usuários.

--- resources/views/pages/lista-usuarios.blade.php
+++ resources/views/pages/lista-usuarios.blade.php
@@ -2,8 +2,36 @@
 <html>
 <head>
     <title>Lista de Usuarios</title>
+    <link rel="stylesheet" href="{{ asset('css/default.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/colors.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    
 </head>
 <body>
-    <h1>Página Contendo lista de Usuários Cadastrados</h1>
+    
+    <div>
+        <table class="table text-center" text-center">
+            <tr class="column">
+                <th class = "px-3 py-3 fs-3">ID</th>
+                <th class = "px-3 py-3 fs-3">NOME</th>
+                <th class = "px-3 py-3 fs-3">E-MAIL</th>
+                <th class = "px-3 py-3 fs-3">TELEFONE</th>
+                <th class = "px-3 py-3 fs-3">CPF/CNPJ</th>
+                <th class = "px-3 py-3 fs-3">ATUAÇÃO</th>
+            </tr>
+            <tr id="rows" class="">
+                <td>{{ $teste = "===" }}</td>
+                <td>{{ $teste = "===" }} </td>
+                <td>{{ $teste = "===" }} </td>
+                <td>{{ $teste = "===" }} </td>
+                <td>{{ $teste = "===" }} </td>
+                <td>{{ $teste = "===" }} </td>
+            </tr>
+        </table>
+    </div>
+    
+    
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ Route::view('/cadastro', 'pages.cadastro')->name('cadastro');
 Route::view('/dashboard', 'pages.dashboard')->name('dashboard');
 Route::view('/perfil/editar', 'pages.edicao-perfil')->name('edicao-perfil');
 Route::view('/perfil', 'pages.visualizacao-perfil')->name('visualizacao-perfil');
-Route::view('/usuarios', 'pages.lista-usuarios')->name('lista-usuarios');
+Route::view('/avaliacoes', 'pages.lista-avaliacao-servicos')->name('lista-avaliacoes');
 
 Route::get('/sobre-nos', function(){
 
@@ -21,7 +21,12 @@ Route::get('/termos', function(){
 
 })->name('termos');
 
-Route::view('/avaliacoes', 'pages.lista-avaliacao-servicos')->name('lista-avaliacoes');
+Route::get('/lista', function(){
+
+    return view('pages/lista-usuarios');
+
+})->name('lista');
+
 
 Route::get('/', function () {
     return view('welcome');
