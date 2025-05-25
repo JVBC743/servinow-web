ef64d97f57476683e5a83a9950ce972abab0dab5
KarmaWT <mateus4pantoja@gmail.com>
2025-05-25 19:15:21 -0400

    Navbar atualizada com bootstrap pra fica responsiva

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,15 +1,34 @@
-<!-- NAVBAR -->
-<nav class="navbar">
-    <div class="navbar-left">
-        <img class="navbar-logo" src="ServiNow-Logo.png" alt="Logo" />
-        <ul>
-            <li><a href="#">Início</a></li>
-            <li><a href="#">Sobre nós</a></li>
-        </ul>
-    </div>
-
-    <div class="navbar-icons" style="padding-right: 1rem;">
-        <i class="fa-solid fa-bell"></i>
-        <i class="fa-solid fa-user"></i>
-    </div>
-</nav>
+    <!-- NAVBAR -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-info fixed-top shadow">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="ServiNow-Logo.png" alt="Logo" class="navbar-logo">
+            </a>
+    
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent"
+                aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+    
+            <div class="collapse navbar-collapse" id="navbarContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link fs-5" href="#">Início</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link fs-5" href="#">Sobre nós</a>
+                    </li>
+                </ul>
+    
+                <!-- Icons -->
+                <div class="d-flex align-items-center">
+                    <a href="#" class="nav-link text-white fs-5 me-3">
+                        <i class="fa-solid fa-bell"></i>
+                    </a>
+                    <a href="#" class="nav-link text-white fs-5">
+                        <i class="fa-solid fa-user"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </nav>
