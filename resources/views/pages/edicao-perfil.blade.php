ad0d58dc33b159d2bef9fa063afb2f713613aa04
jb <jb@jb-desktop-debian.jb-desktop-debian.com.br>
2025-05-26 18:42:21 -0400

    Aplicado o flex-wrap nos elementos desejados e definindo os tamanhos para deixar a tela de edição de perfil responsivo.

--- resources/views/pages/edicao-perfil.blade.php
+++ resources/views/pages/edicao-perfil.blade.php
@@ -4,18 +4,22 @@
     <title>Edição do Perfil</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/default.css') }}">
-    <link rel="stylesheet" href="{{  asset('css/photos.css') }}">
-    <link rel="stylesheet" href="{{  asset('css/sizes.css') }}">
-
-
+    <link rel="stylesheet" href="{{ asset('css/photos.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/sizes.css') }}">
 </head>
 <body id="body">
 
     <h1 class="ms-5 mt-4">Configuração de Conta</h1>
 
+    {{-- 670x727px --}}
+    {{-- 359x727px --}}
+    {{-- 330x727px --}}
+    {{-- RESPONSIVIDADE VIA MEDIA QUERIES --}}
+    
+
     <form action="">
-        <div class="d-flex">
-            <div class="d-flex fs-5">
+        <div class="d-flex flex-wrap">
+            <div class="d-flex fs-5 flex-wrap me-5">
                 <div class="my-3 mx-5 inputs">
                     <input class = "mb-3" type="text" placeholder="Nome"><br>
                     <input class = "mb-3" type="text" placeholder="E-mail"><br>
