aff4c713526299364f249eb7d173edc5279ca3de
KarmaWT <mateus4pantoja@gmail.com>
2025-06-01 20:53:21 -0400

    Card de serviço modificado, deu uma dor de cabeça mas ficou bom

--- resources/views/components/card-servico.blade.php
+++ resources/views/components/card-servico.blade.php
@@ -1,18 +1,22 @@
-<div class="card-servico  card shadow-sm mb-3 mx-auto" style="width: 400px;">
-    <div
-        class="card-servico-header d-flex justify-content-between align-items-center bg-info text-white p-3 rounded-top">
-        <div class="nome-servico fs-5 fw-bold mb-0">
-            {{ $nomeServico }}
-        </div>
-        <!-- Card da foto do serviço -->
-        <div class="card-foto d-flex align-items-center justify-content-center bg-secondary rounded"
-            style="width: 120px; aspect-ratio: 1 / 1;">
-            <img src="{{ asset($imagemServico) }}" class="foto-servico img-fluid rounded" alt="Foto do serviço">
-        </div>
-    </div>
+@props([
+    'imageUrl' => '',
+    'price' => 'R$ 00,00',
+    'title' => 'Título do serviço',
+    'description' => 'Descrição básica sobre o tipo do serviço a ser agendado, da disponibilidade e outras especificações.',
+    'buttonText' => 'Contratar',
+    'buttonUrl' => '#'
+])
 
-    <!-- Descrição do serviço -->
-    <div class="card-body p-3">
-        {{ $descricaoServico }}
+<div class="custom-card" {{ $attributes }}>
+    <div class="card-img-container">
+        <img src="{{ $imageUrl }}" alt="{{ $title }}" class="card-img-top">
     </div>
-</div>
+    <div class="price-tag">{{ $price }}</div>
+    <div class="card-body">
+        <h5 class="card-title">{{ $title }}</h5>
+        <p class="card-text">{{ $description }}</p>
+    </div>
+    <div class="card-footer">
+        <a href="{{ $buttonUrl }}" class="btn-custom">{{ $buttonText }}</a>
+    </div>
+</div>  
