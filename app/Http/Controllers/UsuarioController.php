02e320628a0950c639d4704fe9c72a28deecd7c0
João Victor Brum de Castro <joaovictor.brumc@gmail.com>
2025-06-11 17:13:22 -0400

    Feito o erro da variável não definida ser resolvido.

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -11,8 +11,7 @@ use App\Application\UseCases\ListarFormacoesUseCase;
 use App\Models\Usuario;
 use App\Models\Formacao;
 use App\Http\Requests\EditarUsuarioRequest;
-
-
+use App\Domain\Repositories\UsuarioRepositoryInterface as UsrRepo;
 
 class UsuarioController extends Controller {
 
@@ -22,8 +21,6 @@ class UsuarioController extends Controller {
     }
 
     public function listFormations(ListarFormacoesUseCase $listaFormacoes, $id){
-
-
 
         $lista = $listaFormacoes->execute();
 
@@ -39,18 +36,24 @@ class UsuarioController extends Controller {
     public function show($id){ //método para passar o id do usuário pela URL para simular um login. Se quiser tirar para implementar o login, pode tirar
 
         $editarUsuario = Usuario::find($id);
+
+        if(!$usr){
+            return redirect()->back()->with('error', 'Usuário não encontrado.');
+        }
         $obj_formacao = Formacao::find($editarUsuario->area_atuacao); //Você só tem que arrumar um jeito de implementar isso sem o método "show".
         $editarUsuario->area_atuacao = $obj_formacao->formacao;
 
         return view("pages.edicao-perfil", compact('editarUsuario'));
     }
 
-    public function edit(EditarUsuarioRequest $request, EditarUsuarioUseCase $useCase, int $id){
+    public function edit(EditarUsuarioRequest $request, EditarUsuarioUseCase $useCase,  int $id){
 
         $usr = Usuario::find($id);
         if(!$usr){
             return redirect()->back()->with('error', 'Usuário não encontrado.');
         }
+
+        $lista = (new ListarFormacoesUseCase(app(UsrRepo::class)))->execute();
 
         $data = $request->validated();
 
@@ -61,7 +64,7 @@ class UsuarioController extends Controller {
             return redirect()->back()->with('error', 'Erro ao carregar dados do usuário para edição.');
         }
 
-        return view('pages.edicao-perfil', compact('editarUsuario'));
+        return view('pages.edicao-perfil', compact('editarUsuario', 'lista'));
 
     }
 
