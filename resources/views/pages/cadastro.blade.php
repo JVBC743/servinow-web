c42c311656a8ba20b181ea7dc24586b32e77cf73
manoeljma <manoelmaguiar@gmail.com>
2025-06-11 23:25:40 -0400

    fix: Corrige conflito causado por configuração divergente nos arquivos docker-compose.yml e .env (configuração do banco de dados e permissões do Sail

--- resources/views/pages/cadastro.blade.php
+++ resources/views/pages/cadastro.blade.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tela de Cadastro</title>
-    <link rel="stylesheet" href="style.css"> </head>
+    <title>Cadastro</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Seu CSS -->
+    <link href="{{ asset('css/fonts/style.css') }}" rel="stylesheet">
+</head>
 <body>
     <div class="container-principal">
         <div class="container-formulario">
