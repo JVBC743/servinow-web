830012a4dc103ff20efb6f9662c87c022da173e5
João Victor Brum de Castro <joaovictor.brumc@gmail.com>
2025-06-15 18:45:37 -0400

    Alinhando as coisas na página de cadastro de serviço

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -6,7 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }}</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('css/colors.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/default.css') }}">
-</head>
-<body>
+    <link rel="stylesheet" href="{{ asset('css/photos.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/sizes.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/buttons.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/colors.css') }}">
+
--- resources/views/pages/cadastro-servico.blade.php
+++ resources/views/pages/cadastro-servico.blade.php
@@ -1,11 +1,38 @@
 <x-header :title="'Cadastar Serviço'" />
 
-    <h1>Cadastrar serviço</h1>
+    <h1 class="text-center emphasis_text my-3">Cadastrar serviço</h1>
     <div>
         <form action="{{ route('cadastro.servico.store') }}" method="post">
 
 
+
+            <div class="mx-5 mb-5">
+
+                <div class="inputs">
+                    <input class="mt-5 mb-3" type="text" name="nome_servico" placeholder="Insira o título do servico">
+                </div>
+
+
+                <div class="d-flex mt-3">
+                    <div class="me-5">
+                        <input alt="Enviar imagem"type="image" class = "img_input" style="width: 150px; height: 30px">
+                    </div>
+                    <div class="">
+                        <select name="" id="">
+                            <option value="">Escolha a categoria</option>
+                            {{-- @foreach ( as )
+                                
+                            @endforeach --}}
+                        </select>
+                    </div>
+                </div>
+
+
+            </div>
         </form>
+
+
     </div>
+
 
 <x-footer />
