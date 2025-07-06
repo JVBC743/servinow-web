26e92910848815db3e731ef74a3349fba9587c5a
José Claion <Joseclaionmartins@gmail.com>
2025-07-06 15:40:10 -0400

    correção models

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -2,13 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Categoria extends Model
 {
+    use HasFactory;
+
     protected $table = 'Categoria';
 
     protected $fillable = [
-        'categoria',
+        'nome',
     ];
 }
