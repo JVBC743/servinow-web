af2763ef17fcf31cf3b4d6900b0ab37034325c29
KarmaWT <mateus4pantoja@gmail.com>
2025-07-06 18:12:03 -0400

    tentando ajusta essa merda dessa seach bar, mas n deu

--- resources/views/components/search-bar.blade.php
+++ resources/views/components/search-bar.blade.php
@@ -2,11 +2,13 @@
     'action' => '#',
 ])
 
-<div class="container my-3 search-bar-container">
-    <form action="{{ $action }}" method="GET" class="search-bar d-flex mx-auto">
-        <input type="text" class="form-control search-input flex-grow-1" placeholder="Pesquisar..." name="search">
-        <button type="submit" class="search-button d-flex align-items-center justify-content-center ms-2">
+<div class="search-bar-container"> 
+    <form action="{{ $action }}" method="GET" class="search-bar">
+        <input type="text" class="form-control search-input" 
+               placeholder="Pesquisar..." name="search">
+        <button type="submit" class="search-button">
             <i class="fa-solid fa-magnifying-glass"></i>
         </button>
     </form>
 </div>
+
--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -1,5 +1,26 @@
 @extends ('layouts.autenticado')
 @section('title', 'Dashboard')
 @section('content')
-<h1>Dashboard</h1>
+
+
+<div class="d-flex align-items-center justify-content-between gap-2"> 
+    <h1 class="m-0">Serviços Disponíveis</h1>
+    <div class="flex-grow-1 ms-3" style="max-width: 400px;">
+        <x-search-bar />
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 @endsection
