c97994d046e312fb482e5444d2a4c6926c7f2192
KarmaWT <mateus4pantoja@gmail.com>
2025-07-05 21:21:12 -0400

    correção de telas, criação de layout

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -1,15 +1,6 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - ServiNow</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-</head>
-
-<body>
+@extends ('layouts.app')
+@section('title', 'Login')
+@section('content')
     <div class="container-fluid bg-gradient d-flex justify-content-center align-items-center min-vh-100">
         <div class="main-box d-flex flex-wrap">
             <!-- Coluna Esquerda -->
@@ -53,9 +44,4 @@
             </div>
         </div>
     </div>
-
-    <!-- Ícones Bootstrap -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-</body>
-
-</html>
+@endsection
