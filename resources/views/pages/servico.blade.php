c5e78c17230e61d3efdbd3760739f3008de228bf
João Victor Brum de Castro <joaovictor.brumc@gmail.com>
2025-07-10 17:07:47 -0400

    Feita a parte da foto de servico

--- resources/views/pages/servico.blade.php
+++ resources/views/pages/servico.blade.php
@@ -3,15 +3,28 @@
 @section('title', 'Agendar Serviço')
 
 @section('content')
+                        {{-- {{ dd($servico->caminho_foto) }} --}}
 
-            <div class="row shadow mt-3 mb-5" style="height: 100%; min-height: 700px">
+
+            <div class="row shadow mt-3 mb-5 me-5" style="height: 100%; min-height: 700px;">
 
                 <h1 class="text-center mt-5"> {{ $servico->nome_servico }} </h1>
                 <div class="col-12 col-md-4 d-flex justify-content-center align-items-center">
                     <div class="text-center margin_div_servico">
-                        <div>
-                            <img src="https://static.wixstatic.com/media/1233ff_ca96ec225309492dbd2cef0b7ca9938f~mv2.jpg/v1/fill/w_740,h_493,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/1233ff_ca96ec225309492dbd2cef0b7ca9938f~mv2.jpg" alt="Foto do serviço" class="" style="width: 300px; height: 100px">
-                        </div>
+
+                        @if($servico->caminho_foto)
+                            <div>
+                                <img class="service_photo" src="{{ asset("$servico->caminho_foto") }}" alt="Foto do serviço"> <!-- TIRAR O ASSET PARA PUXAR O CAMIHNO DO MINIO
+                                                                                                                                        A CONSULTA NO BANCO TÁ CERTA -->
+                            </div>
+                        @else
+                            <div>
+                                <img class="service_photo" src="{{ asset('images/servico-nulo.png') }}" alt="Não há foto de serviço">
+                            </div>
+
+                        @endif
+
+
                         <div class="my-5">
                             <label for="data" class="form-label fw-bold">Selecione a Data:</label>
                             <input type="date" id="data" name="data" class="form-control text-cente" style="">
@@ -52,16 +65,17 @@
             <h1 class="text-center">Avaliações</h1>
             <button class="btn btn-info text-white">Enviar avaliação</button>
             {{-- MODAL AQUI --}}
-            <div class="container-fluid shadow">
-
-                
-                    <x-card-avaliacao 
-                    profileImage="{{ asset('images/claion.png') }}"
-                    title="Me dá licença"
-                    userName="José"
-                    rating="5"
-                    description="Preciso rever minhas amizades."
+            <div class="container-fluid shadow mt-3 me-4">
+                <div class="mt-4">
+                    <x-card-avaliacao  
+                        profileImage="{{ asset('images/claion.png') }}"
+                        title="Me dá licença"
+                        userName="José"
+                        rating="5"
+                        description="Preciso rever minhas amizades."
                     />
+                </div>
+                    
 
             </div>
 
