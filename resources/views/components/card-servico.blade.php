6fde1435e5f39dc06ab7e82975fc8ac4769bcd5c
KarmaWT <mateus4pantoja@gmail.com>
2025-07-10 19:25:28 -0400

    Add categoria no Card de serviço

--- resources/views/components/card-servico.blade.php
+++ resources/views/components/card-servico.blade.php
@@ -2,6 +2,7 @@
     'imageUrl' => 'https://secohtuh-es.org.br/wp-content/uploads/2024/04/Seguranca-no-Trabalho-Secohtuh.png',
     'price' => 'R$ 00,00',
     'title' => 'Título do serviço',
+    'category' => 'Indefinido',
     'description' => 'Descrição básica sobre o tipo do serviço a ser agendado, da disponibilidade e outras especificações.',
     'buttonText' => 'Agendar',
     'buttonUrl' => '#'
@@ -13,6 +14,7 @@
     </div>
     <div class="card-body">
         <h5 class="card-title">{{ $title }}</h5>
+        <h5 class="service-category">{{ $category }}</h5>
         <p class="card-text">{{ $description }}</p>
     </div>
     <div class="card-footer">
