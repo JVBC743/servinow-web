b008cb794958192115cfc77b380e92fa8ee67929
KarmaWT <mateus4pantoja@gmail.com>
2025-07-10 20:20:02 -0400

    teste no card

--- resources/views/components/card-servico.blade.php
+++ resources/views/components/card-servico.blade.php
@@ -2,19 +2,23 @@
     'imageUrl' => 'https://secohtuh-es.org.br/wp-content/uploads/2024/04/Seguranca-no-Trabalho-Secohtuh.png',
     'price' => 'R$ 00,00',
     'title' => 'Título do serviço',
+    'category' => 'Indefinido',
     'description' => 'Descrição básica sobre o tipo do serviço a ser agendado, da disponibilidade e outras especificações.',
     'buttonText' => 'Agendar',
     'buttonUrl' => '#'
 ])
+
 
 <div class="custom-card" {{ $attributes }}>
     <div class="card-img-container">
         <img src="{{ $imageUrl }}" alt="{{ $title }}" class="card-img-top">
     </div>
     <div class="card-body">
-        <h5 class="card-title">{{ $title }}</h5>
-        <p class="card-text">{{ $description }}</p>
-    </div>
+    <h5 class="card-title">{{ $title }}</h5>
+        <h6 class="text-muted">{{ $category }}</h6>
+    <p class="card-text">{{ $description }}</p>
+</div>
+
     <div class="card-footer">
         <a href="{{ $buttonUrl }}" class="btn-custom">{{ $buttonText }}</a>
     </div>
